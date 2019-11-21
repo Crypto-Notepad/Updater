9c4d9fd31391ad4e3fac5fa2ce87900dd921b2c5
Alexander <sigmanor@gmail.com>
2019-11-21 21:37:55 +0200

    Update UpdateForm.Designer.cs

--- Updater/UpdateForm.Designer.cs
+++ Updater/UpdateForm.Designer.cs
@@ -28,15 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
             this.descriptionLabel = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.OkButton = new System.Windows.Forms.Button();
-            this.RlsNotesLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.updaterPictureBox = new System.Windows.Forms.PictureBox();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.releaseNotesLabel = new System.Windows.Forms.LinkLabel();
             this.isAppRunningTimer = new System.Windows.Forms.Timer(this.components);
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.updaterPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // descriptionLabel
@@ -51,23 +51,21 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.Window;
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.updaterPictureBox);
             this.panel1.Controls.Add(this.descriptionLabel);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(294, 73);
-            this.panel1.TabIndex = 4;
+            this.panel1.TabIndex = 3;
             // 
-            // pictureBox1
+            // updaterPictureBox
             // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Image = global::Updater.Properties.Resources.arrow_down;
-            this.pictureBox1.Location = new System.Drawing.Point(6, 6);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(60, 60);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 0;
+            this.updaterPictureBox.Image = global::Updater.Properties.Resources.arrow_down;
+            this.updaterPictureBox.Location = new System.Drawing.Point(6, 6);
+            this.updaterPictureBox.Name = "updaterPictureBox";
+            this.updaterPictureBox.Size = new System.Drawing.Size(60, 60);
+            this.updaterPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.updaterPictureBox.TabIndex = 0;
             this.updaterPictureBox.TabStop = false;
             // 
             // updateButton
@@ -95,15 +93,6 @@
             this.releaseNotesLabel.Text = "View release notes";
             this.releaseNotesLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.ReleaseNotesLabel_LinkClicked);
             // 
-            this.RlsNotesLinkLabel.AutoSize = true;
-            this.RlsNotesLinkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.RlsNotesLinkLabel.LinkBehavior = System.Windows.Forms.LinkBehavior.AlwaysUnderline;
-            this.RlsNotesLinkLabel.Location = new System.Drawing.Point(3, 83);
-            this.RlsNotesLinkLabel.Name = "RlsNotesLinkLabel";
-            this.RlsNotesLinkLabel.Size = new System.Drawing.Size(110, 15);
-            this.RlsNotesLinkLabel.TabIndex = 6;
-            this.RlsNotesLinkLabel.Text = "View release notes";
-            this.RlsNotesLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.RlsNotesLinkLabel_LinkClicked);
             // isAppRunningTimer
             // 
             this.isAppRunningTimer.Enabled = true;
